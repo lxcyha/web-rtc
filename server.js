a87eacdb6e3c64ec40814a3dc2e13bd557f03ad8
chenyanghua <chenyanghua@mudu.tv>
2018-12-11 16:24:41 +0800

    file transfer

--- server.js
+++ server.js
@@ -17,11 +17,17 @@ app.get('/data-channel',function (req, res) {
 })
 
 
+app.get('/transfer-file',function (req, res) {
+    res.render('transfer-file.ejs')
+})
+
+
 app.io.route('ready', function (req) {
     req.io.join(req.data.chat_room)
     req.io.join(req.data.signal_room)
+    req.io.join(req.data.files_room)
     app.io.room(req.data.chat_room).broadcast('announce',{
-        message: 'New client in the '+ req.data.chat_room + 'room.'
+        message: 'New client in the '+ req.data.chat_room + ' room.'
     })
 })
 
@@ -32,10 +38,18 @@ app.io.route('send', function (req) {
     })
 })
 
+
 app.io.route('signal', function (req) {
     req.io.room(req.data.room).broadcast('signaling_message',{
         type: req.data.type,
         message: req.data.message
+    })
+})
+
+app.io.route('files', function (req) {
+    req.io.room(req.data.room).broadcast('files',{
+        filename: req.data.filename,
+        filesize: req.data.filesize
     })
 })
 
